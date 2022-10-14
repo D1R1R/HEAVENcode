277dbb860a7c0099b48943fdaffb2e4067d9a0d2
DR <104569323+D1R1R@users.noreply.github.com>
2022-10-14 17:18:48 +0300

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,9 +1,9 @@
 import sys
 
-from PyQt5 import uic  # Импортируем uic
+from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
-import webbrowser  # для открытия сайта
-
+import os
+import qrc_resources
 
 class MyWidget(QMainWindow):
     
@@ -14,12 +14,50 @@ class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('gui_qt_designer.ui', self)  # Загружаем дизайн
+        # -------------------
+        self._click_counter = 0
         self.buttons()
         
     def buttons(self):
-        self.git_hub.clicked.connect(lambda: webbrowser.open('http://www.google.com'))
+        self.git_hub.clicked.connect(lambda: os.system("start \"\" https://github.com/D1R1R/HEAVENcode"))
         # при нажатии на кнопку открыть сайт.
+        self.settings_btn.clicked.connect(self.open_settings_menu)
+        self.prog_btn.clicked.connect(self.open_programm_menu)
+        self.code_btn.clicked.connect(self.open_code_menu)
 
+    def close_all_fr(self):
+        self.code_set_fr.setMaximumWidth(0)
+        self.Always_on_fr.setMaximumWidth(0)
+        self.programm_fr.setMaximumWidth(0)
+        self.settings_fr.setMaximumWidth(0)
+
+    def open_settings_menu(self):
+        self.close_all_fr()
+        if self._click_counter != 1:
+            self.settings_fr.setMaximumWidth(16777215)
+            self._click_counter = 1
+        else:
+            self.Always_on_fr.setMaximumWidth(16777215)
+            self._click_counter = 0
+            
+    def open_programm_menu(self):
+        self.close_all_fr()
+        if self._click_counter != 2:
+            self.programm_fr.setMaximumWidth(16777215)
+            self._click_counter = 2
+        else:
+            self.Always_on_fr.setMaximumWidth(16777215)
+            self._click_counter = 0
+
+    def open_code_menu(self):
+        self.close_all_fr()
+        if self._click_counter != 3:
+            self.code_set_fr.setMaximumWidth(16777215)
+            self._click_counter = 3
+        else:
+            self.Always_on_fr.setMaximumWidth(16777215)
+            self._click_counter = 0
+        
 
 class CodeHighlighting():
     
@@ -35,6 +73,7 @@ class CodeHighlighting():
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    #  app.setStyleSheet(Stylesheet) 
     ex = MyWidget()
     ex.show()
     sys.exit(app.exec_())
